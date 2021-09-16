c41c38a2f3c2069a68fb383c0e364a9fe47dc7f6
Kelvin-rnov <kelvin.rnov@gmail.com>
2021-09-16 16:58:27 -0300

    Requisitos da atividade

--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -3,12 +3,28 @@ import './style.css';
 
 function Cards(){
     return(
-        <div className="card-container">
-            <h2>Título</h2>
-            <p><strong>Duração: </strong></p>
-            <p><strong>Preço: </strong></p>
-            <p><em>IMAGEM</em></p>
-            <p><strong>Descrição: </strong>Reference site about Lorem Ipsum, giving information on its origins, as well as a random Lipsum generator.</p>
+        <div>
+            <div className="card-container">
+                <h2>CLP SIEMENS - STEP 7</h2>
+                <p><strong>Duração: </strong>40 horas</p>
+                <p><strong>Preço: </strong>a verificar</p>
+                <img src="https://alusolda.com.br/wp-content/uploads/2018/07/400_F_7318687_Ey4wdKiMY2ximHZfgWfnymlV5e3gUU2Z.jpg" width="350px"></img>
+                <p><strong>Descrição: </strong>O Curso de CLP SIEMENS STEP 7 tem por objetivo o desenvolvimento de competências relativas à capacitação em visão e aquisição de conhecimentos e habilidades das características técnicas de funcionamento, instalação e aplicação de Controladores Lógicos SIEMENS STEP 7 em sistemas de intertravamento, controle discreto e analógico utilizados nas plantas industriais. Seguindo normas técnicas, ambientais, de saúde e segurança no trabalho.</p>
+            </div>
+            <div className="card-container">
+                <h2>SOLDAGEM ELETRODO REVESTIDO EM AÇO CARBONO</h2>
+                <p><strong>Duração: </strong>36 horas</p>
+                <p><strong>Preço: </strong>a verificar</p>
+                <img src="https://alusolda.com.br/wp-content/uploads/2018/07/400_F_7318687_Ey4wdKiMY2ximHZfgWfnymlV5e3gUU2Z.jpg" width="350px"></img>
+                <p><strong>Descrição: </strong>O Curso de Aperfeiçoamento Profissional Soldagem Eletrodo Revestido em Aço Carbono tem por objetivo o desenvolvimento de competências relativas ao processo de Soldagem Eletrodo Revestido, permitindo a aquisição de capacidades relativas à soldagem na posição sobre-cabeça em chapas de aço carbono considerando a legislação pertinente, normas técnicas, bem como aspectos de saúde e meio ambiente.</p>
+            </div>
+            <div className="card-container">
+                <h2>TORNEIRO MECÂNICO</h2>
+                <p><strong>Duração: </strong>180 horas</p>
+                <p><strong>Preço: </strong>a verificaer</p>
+                <img src="https://alusolda.com.br/wp-content/uploads/2018/07/400_F_7318687_Ey4wdKiMY2ximHZfgWfnymlV5e3gUU2Z.jpg" width="350px"></img>
+                <p><strong>Descrição: </strong>O Curso de Qualificação Profissional Torneiro Mecânico tem por objetivo o desenvolvimento de competências relativas à operação de torno mecânico convencional de acordo com normas técnicas, ambientais e de segurança do trabalho</p>
+            </div>
         </div>
     );
 }
